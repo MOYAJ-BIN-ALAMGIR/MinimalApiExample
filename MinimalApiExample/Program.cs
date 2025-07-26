d552d3c9522bbc861f6d2d940e678ecce28f89f0
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-07-26 17:46:04 +0600

    Create a Minimal API Action Method in ASP.NET Core

--- MinimalApiExample/Program.cs
+++ MinimalApiExample/Program.cs
@@ -4,4 +4,19 @@ app.MapGet("/", () =>
 {
     return "Hello World ";
 });
+
+app.MapPost("/", () =>
+{
+    return Results.Ok("Call post action method");
+
+});
+app.MapPut("/", () =>
+{
+    return Results.Ok("Call put Action Method");
+});
+app.MapDelete("/", () =>
+{
+    return Results.Ok("Call Delete Action Method");
+});
+
 app.Run();
